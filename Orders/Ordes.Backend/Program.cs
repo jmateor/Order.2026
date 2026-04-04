13dac77ec2e0bb01974abca362cd68c67ffec323
jmateor <josemateo3148@gmail.com>
2026-04-04 00:36:14 -0400

    Create database con el primer entity contries

--- Orders/Ordes.Backend/Program.cs
+++ Orders/Ordes.Backend/Program.cs
@@ -1,11 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Ordes.Backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("Name=LocalConnection"));
 
 var app = builder.Build();
 
