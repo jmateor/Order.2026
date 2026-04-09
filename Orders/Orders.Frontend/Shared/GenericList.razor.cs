d660563441a2226c4aec8c719a9a74393b51c448
jmateor <josemateo3148@gmail.com>
2026-04-09 00:06:43 -0400

    Cambio de codigo

---
+++ Orders/Orders.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace Orders.Frontend.Shared
+{
+    public partial class GenericList<Titem>
+    {
+        [Parameter] public RenderFragment? Loading { get; set; }
+        [Parameter] public RenderFragment? NoRecords { get; set; }
+        [Parameter, EditorRequired] public RenderFragment? Body { get; set; } = null!;
+        [Parameter, EditorRequired] public List<Titem> MyList { get; set; } = null!;
+    }
+}
